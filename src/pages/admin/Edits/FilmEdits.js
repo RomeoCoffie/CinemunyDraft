ef80460a9b8599f65a477de45e27a46f21b0a423
RomeoCoffie <romeocoffie@dextsolutions.com>
2023-06-07 15:59:06 +0000

    film Edit

--- src/pages/admin/Edits/FilmEdits.js
+++ src/pages/admin/Edits/FilmEdits.js
@@ -5,8 +5,11 @@ import Multiselect from 'multiselect-react-dropdown';
 import { genreList, keywordsList } from '../../../data/datalinks';
 import { AiOutlinePlus } from 'react-icons/ai';
 
-import { db } from '../../../components/firebase/config';
+import { db, storage } from '../../../components/firebase/config';
 import { updateDoc, doc } from 'firebase/firestore';
+
+import { getDownloadURL, ref } from '@firebase/storage';
+import { uploadBytesResumable } from 'firebase/storage';
 
 function FilmEdits() {
   const [whereEdit, setWhereEdit]=useState(null)
@@ -18,7 +21,13 @@ function FilmEdits() {
   const [progress, setProgress] = useState(null);
   const [director, setDirector] = useState([]);
   const [cast, setCast] = useState([]);
+  const [dirOption, setDirOption]=useState('');
+  const [castOption, setCastOption]=useState('')
+  const [newGenre, setNewGenre]=useState([])
+  const [newKeywords, setNewKeywords]=useState([])
 
+console.log(director,'direcctorList')
+console.log(dirOption,'option')
 
   let { state }=useLocation();
   let theMovie=state.filmDetails
@@ -41,6 +50,141 @@ function FilmEdits() {
       console.log(error);
     }
   }
+
+  let submitNewGenre= async (newGenre)=>{
+    const theRef = doc(db, 'movies', theMovie.id);
+    console.log(newGenre,'theNewGenre')
+    if (newGenre.length===0 || newGenre===undefined) {
+      alert('Please input a new Genre')
+      return;
+    }
+    try {
+      await updateDoc(theRef, {
+        genre: newGenre
+      });
+      alert('Updated')
+      setWhereEdit(null)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  let submitNewKeywords= async (newKeywords)=>{
+    const theRef = doc(db, 'movies', theMovie.id);
+    console.log(newKeywords,'theNewGenre')
+    if (newKeywords.length===0 || newKeywords===undefined) {
+      alert('Please input a new Keyword')
+      return;
+    }
+    try {
+      await updateDoc(theRef, {
+        keywords: newKeywords
+      });
+      alert('Updated')
+      setWhereEdit(null)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleFileChange = async (e) => {
+    let selected = e.target.files[0];
+    console.log(selected);
+
+    if (selected) {
+      if (!selected.type.includes('image')) {
+        alert('Selected file size must be an image');
+        return;
+      }
+
+      if (selected.size > 9999999) {
+        alert('Selected file size must be less than 1Mb');
+        return;
+      }
+
+      const metadata = {
+        contentType: selected.type,
+        customMetadata: {
+          title: theMovie.title,
+          description: theMovie.desc,
+          year: theMovie.year,
+        },
+      };
+
+      const storageRef = ref(storage, `/movies/${theMovie.title}/${selected.name}`);
+      const storageUpload = uploadBytesResumable(
+        storageRef,
+        selected,
+        metadata
+      );
+
+      storageUpload.on(
+        'state_changed',
+        (snapshot) => {
+          const prog = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+          setProgress(prog);
+          console.log('upload is' + progress + '% done');
+        },
+        (err) => console.log(err),
+        () => {
+          getDownloadURL(storageUpload.snapshot.ref).then(async (url) => {
+            const theRef = doc(db, 'movies', theMovie.id);
+            try {
+              await updateDoc(theRef, {
+                movieImgUrl: url
+              });
+              alert('Updated')
+              setWhereEdit(null)
+            } catch (error) {
+              console.log(error);
+            }
+          });
+        }
+      );
+    }
+
+    return;
+  };
+
+
+  let submitNewCast= async (cast)=>{
+    const theRef = doc(db, 'movies', theMovie.id);
+    console.log(cast,'theNewCast')
+    if (cast==='' || cast===undefined) {
+      alert('Please input a new Title')
+      return;
+    }
+    try {
+      await updateDoc(theRef, {
+        cast: cast
+      });
+      alert('Updated')
+      setWhereEdit(null)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  let submitNewDir= async (director)=>{
+    const theRef = doc(db, 'movies', theMovie.id);
+    console.log(director,'theNewDirectorList')
+    if (director==='' || director===undefined) {
+      alert('Please input a new Title')
+      return;
+    }
+    try {
+      await updateDoc(theRef, {
+        director: director
+      });
+      alert('Updated')
+      setWhereEdit(null)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   let submitNewDesc= async (newDescription)=>{
     const theRef = doc(db, 'movies', theMovie.id);
     console.log(newDescription,'theNewDesc')
@@ -117,15 +261,20 @@ function FilmEdits() {
     console.log('enteredEditBlock')
     if (whereEdit==='keywords') {
            return (<div>
+           <div>
             <Multiselect
             isObject={false}
-            selectedValues={[]}
-            /*  onKeyPressFn={(e) => console.log(e.target.value)} */
-            // onRemove={(e) => setContentIndex(e)}
-            // onSearch={(e) => console.log(e.target.value)}
-            // onSelect={(e) => setContentIndex(e)}
+            onRemove={(e) => {setNewKeywords(e); console.log(newKeywords)}}
+            onSelect={(e) => {setNewKeywords(e); console.log(newKeywords)}}
             options={keywordsList}
           />
+          </div>
+          <div>
+            {
+              newKeywords.length>0 ?
+              (<button onClick={(e)=>{submitNewKeywords(newKeywords)}}>Submit New Keywords</button>) : <label>Add a Keyword</label>
+            }
+          </div>
           </div>)
     } else if (whereEdit==='description'){
       return (
@@ -137,34 +286,51 @@ function FilmEdits() {
           <button onClick={(e)=>{submitNewDesc(newDescription)}}>Submit New Description</button>
       </div>)
     } else if (whereEdit==='directors'){
-      console.log('enteredDirectors')
-
       return (<div>
-        <input
-            type="text"
-            className={`${
-              director.length < 1 ? 'track-input' : 'track-input1'
-            }`}
-            onChange={(e) => (e.target.value)}
-            // value={newOption}
-            // ref={optionsInput}
-          />
-          <button>
+      <div>
+          <input
+                type="text"        
+                onChange={(e) => setDirOption(e.target.value)}
+              />
+          <button onClick={(e)=>{
+            console.log(dirOption)
+            setDirector(
+            [...director, dirOption]
+          )}}>
             <AiOutlinePlus />
           </button>
+      </div>
+        <div>
+          {
+            director.length>0 ?
+            (
+              director.map((dirValue)=> { return (
+                <div>
+                  <label>{dirValue}</label>
+                </div>
+                )})
+            ) : <label>Add a name</label>
+          }
+        </div>
+        <div>
+          {
+            director.length>0 ?
+            (
+              <button onClick={(e)=>{submitNewDir(director)}}>Submit New Directors</button>
+            ) : <label></label>
+          }
+        </div>
       </div>)
     } else if (whereEdit==='image'){
       console.log('enteredImage')
 
       return (<div>
-        <div>{theMovie.movieImgUrl}</div>
+        <div><img src={theMovie.movieImgUrl} alt='' /></div>
         <div>
         <input
                 type="file"
-                // onChange={handleFileChange}
+                onChange={handleFileChange}
                 accept="image/*"
-                required
-                // ref={inputClear}
               />
               {progress && (
                 <span style={{ color: 'red' }}>
@@ -176,28 +342,60 @@ function FilmEdits() {
     }else if (whereEdit==='cast'){
       console.log('enteredCast')
       return (<div>
+      <div>
         <input
             type="text"
-            className={`${cast.length < 1 ? 'track-input' : 'track-input1'}`}
-            onChange={(e) => (e.target.value)}
-            // value={newCast}
-            // ref={castInput}
+            onChange={(e) => setCastOption(e.target.value)}
           />
-          <button>
+          <button onClick={(e)=>{
+            console.log(castOption)
+            setCast(
+            [...cast, castOption]
+          )}}>
             <AiOutlinePlus />
           </button>
-      </div>)
+          </div>
+          <div>
+          {
+            cast.length>0 ?
+            (
+              cast.map((castValue)=> { return (
+                <div>
+                  <label>{castValue}</label>
+                </div>
+                )})
+            ) : <label>Add a name</label>
+          }
+        </div>
+        <div>
+          {
+            cast.length>0 ?
+            (
+              <button onClick={(e)=>{submitNewCast(cast)}}>Submit New Directors</button>
+            ) : <label></label>
+          }
+        </div>
+      </div>
+      )
     } else if (whereEdit==='genre'){
       console.log('enteredgenre')
-
       return (
       <div>
-      <Multiselect
+      <div>
+        <Multiselect
             isObject={false}
-            // onRemove={(e) => setGenre(e)}
-            // onSelect={(e) => setGenre(e)}
+            onRemove={(e) => {setNewGenre(e); console.log(newGenre)}}
+            onSelect={(e) => {setNewGenre(e); console.log(newGenre)}}
             options={genreList}
           />
+      </div>
+      <div>
+        {
+          newGenre.length>0 ?
+          (<button onClick={(e)=>{submitNewGenre(newGenre)}}>Submit New Genre List</button>) : <label>Add a Genre</label>
+        }
+      </div>
+      
       </div>)
     } else if (whereEdit==='rating'){
       return (
@@ -205,13 +403,11 @@ function FilmEdits() {
       <div>
           Rating: {theMovie.rating}
         </div>
-        <form>
           <input type="number"
             min="1"
             max="10"
             step="any" onChange={(e)=>{setNewRating(e.target.value)}} placeholder='New Rating' />
           <button onClick={(e)=>{submitNewRating(newRating)}}>Submit New Rating</button>
-        </form>
       </div>)
     } else if (whereEdit==='year'){
       return (
