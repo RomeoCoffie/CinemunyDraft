58d8ce91bd352d5aa19ac40570b3b38941d3fafa
RomeoCoffie <romeocoffie@dextsolutions.com>
2023-06-20 11:07:13 +0000

    pagination for winners

--- src/pages/admin/Lists/WinnersList.js
+++ src/pages/admin/Lists/WinnersList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useCollection } from '../../../Hooks/useCollection';
 import '../admin.css'
 
@@ -6,12 +6,23 @@ import '../admin.css'
 function WinnersList() {
     const { documents: winners } = useCollection('winners', ['createdAt', 'desc']);
     console.log(winners,'theWin')
+    const [currentPage, setCurrentPage]=useState(1)
+    const [winnersPerPage, setWinnersPerPage]=useState(20)
+    const  indexOfLastWinner= currentPage * winnersPerPage
+    const indexOfFirstWinner= indexOfLastWinner - winnersPerPage
+    const currentWinners=winners.slice(indexOfFirstWinner, indexOfLastWinner)
+
+    const pageNumbers=[]
+    for (let i=1; i <= Math.ceil(winners.length / winnersPerPage); i++){
+        pageNumbers.push(i)
+    }
+    const paginate=(number)=> setCurrentPage(number);
   return (
     <div className='winListPage'>
         {
-            winners.length>0 ?
+            currentWinners.length>0 ?
             (
-                winners.map((winner)=>(
+                currentWinners.map((winner)=>(
                 <div key={winner.user} className='winnerslisttab'>
                     <div><label>Email: </label><span>{winner.email}</span></div>
                     <div><label>Phone Number: </label><span>{winner.value}</span></div>
@@ -20,6 +31,18 @@ function WinnersList() {
                 ))
             ) : <label>Loading...</label>
         }
+        <ul className='pagination'>
+            {
+                pageNumbers ?
+                pageNumbers.map(number=>(
+                    <li className='pageNum'>
+                        <a href="#" onClick={()=>paginate(number)}>
+                            {number}
+                        </a>
+                    </li>
+                )) : <label>1</label>
+            }
+        </ul>
     </div>
   )
 }
